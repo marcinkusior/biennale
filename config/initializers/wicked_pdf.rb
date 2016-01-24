8e494478807ae6b72e8ebef633738ebaee09f40a
q510r <kusior.marcin@gmail.com>
2016-01-24 22:25:03 +0100

    changed wickedpdf path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,8 +2,8 @@ WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
  
-  :exe_path => '/home/mkusior/biennale/vendor/bundle/ruby/2.1.0/bin/wkhtmltopdf' 
+  #:exe_path => '/home/mkusior/biennale/vendor/bundle/ruby/2.1.0/bin/wkhtmltopdf' 
   #localpath
-  #:exe_path => '/home/marcin/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
+  :exe_path => '/home/marcin/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
 }
 	
